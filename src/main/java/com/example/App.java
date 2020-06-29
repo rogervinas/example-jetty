64c4593576815b536b0d22e75b191b2cfc8da1cf
Anton Kozlov <akozlov@azul.com>
2020-06-29 13:58:52 +0300

    Add C/RaC support

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -5,18 +5,49 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 import java.io.IOException;
 
+// org.crac could be used instead of jdk.crac
+// https://github.com/CRaC/docs#orgcrac
+import jdk.crac.Context;
+import jdk.crac.Core;
+import jdk.crac.Resource;
+
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
-class ServerManager
+class ServerManager implements Resource
 {
     Server server;
+    Thread preventExitThread;
 
     public ServerManager(int port, Handler handler) throws Exception {
         server = new Server(8080);
         server.setHandler(handler);
+        server.start();
+        Core.getGlobalContext().register(this);
+
+        // beforeCheckpoint implemented in simplest manner: shutdown the jetty
+        // server. There may be no non-daemon threads left, so JVM may exit.
+        // Here we provides a thread that will keep JVM running.
+        preventExitThread = new Thread(() -> {
+            while (true) {
+                try {
+                    Thread.sleep(1_000_000);
+                } catch (InterruptedException e) {
+                }
+            }
+        });
+        preventExitThread.start();
+    }
+
+    @Override
+    public void beforeCheckpoint(Context<? extends Resource> context) throws Exception {
+        server.stop();
+    }
+
+    @Override
+    public void afterRestore(Context<? extends Resource> context) throws Exception {
         server.start();
     }
 }
