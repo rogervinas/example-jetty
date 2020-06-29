7691c1e77715fb14e96b3740d8203f3bf0bb8953
Anton Kozlov <akozlov@azul.com>
2020-06-29 12:00:18 +0300

    Use jetty for example

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,44 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+class ServerManager
 {
-    public static void main( String[] args )
+    Server server;
+
+    public ServerManager(int port, Handler handler) throws Exception {
+        server = new Server(8080);
+        server.setHandler(handler);
+        server.start();
+    }
+}
+
+public class App extends AbstractHandler
+{
+    static ServerManager serverManager;
+
+    public void handle(String target,
+                       Request baseRequest,
+                       HttpServletRequest request,
+                       HttpServletResponse response)
+        throws IOException, ServletException
     {
-        System.out.println( "Hello World!" );
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        baseRequest.setHandled(true);
+        response.getWriter().println("Hello World");
+    }
+
+    public static void main( String[] args ) throws Exception
+    {
+        serverManager = new ServerManager(8080, new App());
     }
 }
